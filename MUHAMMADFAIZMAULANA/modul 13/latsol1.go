dd5c6590fdbca871bd323fac099981358de5acb3
Farel Juliyandra Restu Hermawan <fareljuliyandragntg@gmail.com>
2025-12-13 22:45:45 +0700

    Add files via upload

---
+++ MUHAMMADFAIZMAULANA/modul 13/latsol1.go
@@ -0,0 +1,9 @@
+package main
+
+import ("fmt")
+func main() {
+	var bilangan int
+	fmt.Scan(&bilangan)
+	jumlahDigit := len(fmt.Sprintf("%d", bilangan))
+	fmt.Println("Jumlah digit:", jumlahDigit)
+}
