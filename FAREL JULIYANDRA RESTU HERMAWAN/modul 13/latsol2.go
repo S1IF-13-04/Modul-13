863214c035d53fffe016dbd16fd505c57420966d
Farel Juliyandra Restu Hermawan <fareljuliyandragntg@gmail.com>
2025-12-13 22:53:21 +0700

    Add files via upload

---
+++ FAREL JULIYANDRA RESTU HERMAWAN/modul 13/latsol2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x float64
+	fmt.Scan(&x)
+	batasAtas := float64(int(x)) + 1
+	for v := x + 0.1; v <= batasAtas; v += 0.1 {
+		fmt.Printf("%.1f\n", v)
+	}
+}
