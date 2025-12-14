a37fee2d91dd3eb5118f4818989655979012d386
billaforse <anggarabilla@gmail.com>
2025-12-14 18:42:44 +0700

    Create g1.go

---
+++ NAFISAH SALSABILA/g1.go
@@ -0,0 +1,14 @@
+ackage main
+import "fmt"
+
+func main() {
+	var waktu string
+	var ulang int
+	fmt.Scan(&waktu, &ulang)
+	i ;= 0
+	for kondisi := false; !kondisi; {
+		fmt.Println(waktu)
+		i++
+		kondisi = (i >= ulang)
+	}
+}
